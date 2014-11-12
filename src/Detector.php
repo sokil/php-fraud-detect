04b59bcd1f9ce7c18062a1accaf50bae0f4d4e5e
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-11-12 14:39:19 +0200

    interface

--- src/Detector.php
+++ src/Detector.php
@@ -4,5 +4,30 @@ namespace Sokil\FraudDetector;
 
 class Detector
 {
+    private $passed;
     
+    public function onCheckPassed($callable)
+    {
+        
+    }
+    
+    public function onCheckFailed($callable)
+    {
+        
+    }
+    
+    public function check()
+    {
+        if(null !== $this->passed) {
+            if($this->passed) {
+                // call passed handlers
+            } else {
+                // call failed handlers
+            }
+        }
+        
+        // check
+        
+        // trigger handlers
+    }
 }
