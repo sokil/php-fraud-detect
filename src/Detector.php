32483e104eb0b82eac966810874f251b92000e6a
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-11-12 16:46:54 +0200

    add handlers

--- src/Detector.php
+++ src/Detector.php
@@ -6,23 +6,46 @@ class Detector
 {
     private $passed;
     
+    private $handlers = array(
+        'checkPassed' => array(),
+        'checkFailed' => array(),
+    );
+    
     public function onCheckPassed($callable)
     {
-        
+        return $this->addHandler('checkPassed', $callable);
     }
     
     public function onCheckFailed($callable)
     {
+        return $this->addHandler('checkFailed', $callable);
+    }
+    
+    private function addHandler($name, $callable)
+    {
+        $this->handlers[$name][] = $callable;
+        return $this;
+    }
+    
+    private function callHandlers($name)
+    {
+        foreach($this->handlers[$name] as $callable) {
+            call_user_func($callable);
+        }
         
+        return $this;
     }
     
     public function check()
     {
+        // Check already done. Just call handlers
         if(null !== $this->passed) {
             if($this->passed) {
                 // call passed handlers
+                $this->callHandlers('checkPassed');
             } else {
                 // call failed handlers
+                $this->callHandlers('checkFailed');
             }
         }
         
