dffd6996e255839144b6e05cc6f014293f082dd8
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-11-17 00:09:18 +0200

    fix test

--- tests/DetectorTest.php
+++ tests/DetectorTest.php
@@ -4,7 +4,7 @@ namespace Sokil\FraudDetector;
 
 class DetectorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCheck()
+    public function testCheck_RequestRate()
     {
         $detector = new Detector();
         
@@ -35,5 +35,31 @@ class DetectorTest extends \PHPUnit_Framework_TestCase
         
         $detector->check();
         $this->assertFalse($status->ok);    
+    }
+    
+    public function testCheck_Variable()
+    {
+        $detector = new Detector();
+        
+        $status = new \stdClass();
+        $status->ok = null;
+        
+        $GLOBALS['globalVariable'] = 42;
+        
+        $detector
+            ->setKey('someKey')
+            ->addProcessor('variable', function(\Sokil\FraudDetector\Processor\VariableProcessor $processor) {
+                /* @var $processor \Sokil\FraudDetector\Processor\VariableProcessor */
+                $processor->setName('globalVariable')->equals(42);
+            })
+            ->onCheckPassed(function() use($status) {
+                $status->ok = true;
+            })
+            ->onCheckFailed(function() use($status) {
+                $status->ok = false;
+            })
+            ->check();
+            
+        $this->assertTrue($status->ok);   
     }
 }
