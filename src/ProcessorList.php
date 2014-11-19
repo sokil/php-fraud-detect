ff75c6236bdff3161d4e8b6ab192c01290c1de9e
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-11-19 22:22:30 +0200

    processor list

---
+++ src/ProcessorList.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sokil\FraudDetector;
+
+class ProcessorList
+{
+    public function declareProcessor($name, $callable, $priority = 0)
+    {
+        
+    }
+    
+    public function getProcessor($name)
+    {
+        
+    }
+    
+    public function isProcessorDeclared($name)
+    {
+        
+    }
+}
