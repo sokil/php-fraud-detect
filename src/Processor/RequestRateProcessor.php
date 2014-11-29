b35b9b7380e7658b4df95b907cc4d6751fbc2228
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-11-29 11:42:00 +0200

    check if blacklist configured

--- src/Processor/RequestRateProcessor.php
+++ src/Processor/RequestRateProcessor.php
@@ -5,63 +5,70 @@ namespace Sokil\FraudDetector\Processor;
 class RequestRateProcessor extends \Sokil\FraudDetector\AbstractProcessor
 {
     private $requestNumber = 1;
-    
+
     private $timeInterval = 1;
-    
+
     /**
      *
      * @var \Sokil\FraudDetector\Processor\RequestRate\AbstractCollector
      */
     private $collector;
-    
+
     private $banOnRateExceed = false;
-    
+
     protected function isPassed()
-    {        
+    {
         return !$this->collector->isRateLimitExceed();
     }
-    
+
     protected function afterCheckPassed()
     {
         $this->collector->collect();
         return $this;
     }
-    
+
     protected function afterCheckFailed()
     {
         if($this->banOnRateExceed) {
+
+            // check if black list configured
+            if(!$this->detector->isProcessorDeclared('blackList')) {
+                throw new \Exception('Black list not configured');
+            }
+
+            // add key to black list
             $this->detector
                 ->getProcessor('blackList')
                 ->store($this->detector->getKey());
         }
-        
+
         return $this;
     }
-    
+
     public function banOnRateExceed()
     {
         $this->banOnRateExceed = true;
         return $this;
     }
-    
+
     public function setRequestRate($requestNumber, $timeInterval)
     {
         $this->requestNumber = $requestNumber;
         $this->timeInterval = $timeInterval;
         return $this;
     }
-    
+
     public function setCollector($type, $configuratorCallable = null)
-    {        
+    {
         $className = '\Sokil\FraudDetector\Processor\RequestRate\Collector\\' . ucfirst($type) . 'Collector';
-        
+
         if(!class_exists($className)) {
             throw new \Exception('Collector ' . $type . ' not found');
         }
 
         $this->collector = new $className(
             $this->detector->getKey(),
-            $this->requestNumber, 
+            $this->requestNumber,
             $this->timeInterval
         );
 
@@ -69,8 +76,8 @@ class RequestRateProcessor extends \Sokil\FraudDetector\AbstractProcessor
         if(is_callable($configuratorCallable)) {
             call_user_func($configuratorCallable, $this->collector);
         }
-        
+
         return $this;
-        
+
     }
 }
